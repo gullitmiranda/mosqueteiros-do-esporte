92033a04cbbfa7de20529ad006f580764fe7e6ac
Julio Protzek <julioprotzek@gmail.com>
2012-12-05 14:10:21 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mosqueteiros::Application.config.secret_token = '4b17e4a0ba1fa720484f3d64ffe827c730c0aaa4fcb4c02c5090a5e6cfc92583e6e522ad8d480c2e9824ae8b8586608c8f86e7a0fe5e8cda9a25c15a5b9abbc2'
