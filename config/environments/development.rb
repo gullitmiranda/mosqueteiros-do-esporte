b0b4edbecc423e694502da26618c35f548863359
Renato Carvalho <renatolz@gmail.com>
2012-12-08 21:38:11 -0200

    Ajustes na configuraçao de desenvolvimento e de producao. Ajustes header area do projeto

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,25 +1,14 @@
 Mosqueteiros::Application.configure do
-  
-  # Slim::Engine.set_default_options pretty: true
-  # Slim::Engine.set_default_options :format => :html5
+  Slim::Engine.set_default_options pretty: true
+  Slim::Engine.set_default_options :format => :html5
   config.action_mailer.default_url_options = { host: 'localhost:5000' }
-
-  # Settings specified here will take precedence over those in config/application.rb
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-
-  # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
-
-  # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-$stdout.sync = true
+  $stdout.sync = true
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger
