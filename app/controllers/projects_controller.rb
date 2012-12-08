db1208bb9c00f211dcc3fc322e2a09c41cd45b4e
Renato Carvalho <renatolz@gmail.com>
2012-12-08 20:59:25 -0200

    Implementação do header comum. Ajustes na paginaçao.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,6 @@
 class ProjectsController < ApplicationController
   def index
-    # TODO: atualizar número de items por página
-    @projects = Project.published.ordered_by_date.page(params[:page]).per(2)
+    @projects = Project.published.ordered_by_date.page(params[:page]).per(20)
   end
 
   def show
