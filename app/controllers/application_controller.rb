3bb2e6e2fe136175949f99b14c3cafa00c16c9c7
Julio Protzek <julioprotzek@gmail.com>
2013-01-21 18:07:52 -0200

    http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # http_basic_authenticate_with :name => "mosqueteiros", :password => "dartagnan2012"
+  http_basic_authenticate_with :name => "mosqueteiros", :password => "dartagnan2012"
 
   def not_found
     raise ActionController::RoutingError.new('Not Found')
